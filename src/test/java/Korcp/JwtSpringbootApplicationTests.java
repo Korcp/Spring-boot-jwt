161e8c07224e10a91681f80e956400658194e8a9
박현수 <pm6567@naver.com>
2023-11-11 02:46:40 +0900

    initial commit

---
+++ src/test/java/Korcp/JwtSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package Korcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
